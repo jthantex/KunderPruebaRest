de3e1b90f660ae87cd032beb9ed16b9b5504b523
JonathanTeixeira <jthantex@users.noreply.github.com>
2017-07-13 12:17:18 -0400

    Commit entrega prueba Jonathan Teixeira

---
+++ KunderPruebaRest/KunderPruebaRest/Controllers/timeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KunderPruebaRest.Models;
+using System.Globalization;
+
+namespace KunderPruebaRest.Controllers
+{
+    public class timeController : ApiController
+    {
+        /// <summary> GET: api/time/
+        ///  Ejemplo /api/time?value=2009/03/01T23:57:00-4:00</summary>
+        public IHttpActionResult Get([FromUri]string value)
+        {
+            try
+            {
+                DateTime dateTime;
+                CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+
+                if ((value == null) || !(DateTime.TryParse(value, culture, DateTimeStyles.AdjustToUniversal, out dateTime)))
+                    return BadRequest();
+                
+                Retorno retorno = new Retorno { code = "00", description = "OK", data = dateTime.ToString("HH:mm:ss") };
+
+                return Ok(retorno);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ KunderPruebaRest/KunderPruebaRest/Controllers/wordController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KunderPruebaRest.Models;
+
+namespace KunderPruebaRest.Controllers
+{
+    public class wordController : ApiController
+    {
+        /// <summary> POST: api/word
+        ///  Ejemplo /api/word Content-type: application/json {"data":"kunder"}</summary>
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]Entrada entrada)
+        {
+            try
+            {
+                if ((entrada == null) || !entrada.data.All(c => char.IsLetter(c)))
+                    return BadRequest();
+                
+                string dataUpper = entrada.data.ToUpper();
+                Retorno retorno = new Retorno { code = "00", description = "OK", data = dataUpper };
+
+                return Ok(retorno);
+
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
